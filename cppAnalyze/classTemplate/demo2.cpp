7610fc5f2f34d1f9db34e0e69cd4bcf0e86b72b8
BlackJibert <z1319885580@163.com>
2023-12-31 16:34:29 +0800

    cpp

--- cppAnalyze/classTemplate/demo2.cpp
+++ cppAnalyze/classTemplate/demo2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-void Swap(T& a, T& b)
+template <typename T>
+void Swap(T &a, T &b)
 {
     T temp = a;
-    b=a;
-    a = temp;
+    a = b;
+    b = temp;
 }
 int main()
 {
+
     return 0;
 }
