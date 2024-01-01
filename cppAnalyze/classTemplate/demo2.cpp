c65cd2a4ee521c9b1c36223fce9703e92e6fe6d6
BlackJibert <z1319885580@163.com>
2024-01-01 22:15:59 +0800

    cpp

--- cppAnalyze/classTemplate/demo2.cpp
+++ cppAnalyze/classTemplate/demo2.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-void Swap(T &a, T &b)
+class GGbonds
 {
-    T temp = a;
-    a = b;
-    b = temp;
-}
+public:
+    template <typename T>
+    GGbonds(T a)
+    {
+        cout << "a:" << a << endl;
+    }
+    template <typename T>
+    void show(T b)
+    {
+        cout << "b:" << b << endl;
+    }
+
+    // virtual void show2(T c)
+    // {
+    //     cout << "c:" << c << endl;
+    // }
+};
 int main()
 {
-
+    GGbonds g("蜘蛛侠");
+    g.show(3);
     return 0;
 }
